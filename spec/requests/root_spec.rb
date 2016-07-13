cbb7d424b62dc344457b58c2b0702fec5dcfc6d0
tommy <tommpoland@gmail.com>
2016-07-13 13:36:32 +0200

    more test for root; simplecov added

--- spec/requests/root_spec.rb
+++ spec/requests/root_spec.rb
@@ -1,8 +1,27 @@
 require "web_helper"
 
 RSpec.describe "GET /", type: :request do
-  it "returns 200 HTTP code" do
+  before do
     get "/"
+  end
+
+  it "returns vaild html Content-Type" do
+    expect(last_response.headers["Content-Type"]).to include("text/html")
+  end
+
+  it "returns 200 HTTP code" do
     expect(last_response.status).to eql(200)
+  end
+
+  it "returns title" do
+    expect(last_response.body).to include("<title>Shop<\/title>")
+  end
+
+  it "returns link to basket" do
+    expect(last_response.body).to match("<a\ href=\"\/basket\">.*BASKET.*<\/a>")
+  end
+
+  it "return list of products" do
+    expect(last_response.body).to match("<ul>(\\s*<li>\\s*.*\\s*.*<\/li>\\s*)*<\/ul>")
   end
 end
